afb0c273ded8039b3f5560b29b69d0de0f8892e4
souzapx.david@gmail.com <souzapx.david@gmail.com>
2023-03-15 18:22:41 -0300

    Implementadainterface no programa

--- model/services/RentalServices.java
+++ model/services/RentalServices.java
@@ -10,9 +10,9 @@ public class RentalServices {
 	private Double pricePerHour;
 	private Double pricePerDay;
 
-	private BrazilTaxServices taxService;
+	private TaxService taxService;
 
-	public RentalServices(Double pricePerHour, Double pricePerDay, BrazilTaxServices taxService) {
+	public RentalServices(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
